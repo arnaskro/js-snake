5750cd882cadd1c9677c81a2e63c061e47ab410a
Arnas Kromelis <arnas.kromelis@gmail.com>
2017-05-21 19:14:39 +0200

    game loop & end game detection

--- game.js
+++ game.js
@@ -2,12 +2,27 @@ $(function() {
     var base = $('#app');
     var doc = $(document);
 
+    const STATES = { ACTIVE: 1, PAUSED: 0, LOSSER: 2 };
+    const DIRECTIONS = { UP: 0, DOWN: 1, LEFT: 2, RIGHT: 3 };
+
+    var GRID_SIZE = 14;
+    var GAME_SPEED = 700;
+
+    var STATE = STATES.PAUSED;
+    var DIRECTION = DIRECTIONS.UP;
+    var CURR = { x: 0, y: 0 };
+    var SCORE = 0;
+
+
     $.app = {
-        vars: {
-            gridSize: 14
-        },
         elements: {
             startButton: "<a id='startButton' class='button is-primary is-large'>Start game</a>",
+            heading: function (text) { 
+                return "<p class='title is-1 is-bold'>"+text+"</p>";
+            },
+            score: function (text) { 
+                return "<p class='subtitle is-4'>"+text+"</p>";
+            },
             apple: "<div class='apple'></div>",
             pane: function(x, y) {
                 return "<div class='pane' id='pane-"+x+"-"+y+ "'></div>";
@@ -31,30 +46,45 @@ $(function() {
                 base.html($.app.elements.startButton);
             },
             start: function() {
+                $.app.resetVars();
                 $.app.drawArena();
+
+
+                $.app.addApple(2, 5);
+                $.app.setHead(8, 8);
+
+                STATE = STATES.ACTIVE;
+                $.app.loop();
+            },
+            losser: function() {
+                base.html('');
+                base.append($.app.elements.heading("YOU LOST!"));
+                base.append($.app.elements.score("Your score: " + SCORE));
+                base.append($.app.elements.startButton);
             }
+        },
+        resetVars: function() {
+            SCORE = 0;
+            DIRECTION = DIRECTIONS.UP;
         },
         resetBaseStyles: function() {
             base.attr("class", "is-fullwidth has-text-centered");
         },
         drawArena: function() {
             var panes = "";
-            var size = $.app.vars.gridSize;
 
-            for (var y = 1; y <= size; y++) {
-                for (var x = 1; x <= size; x++) {
+            for (var y = 1; y <= GRID_SIZE; y++) {
+                for (var x = 1; x <= GRID_SIZE; x++) {
                     panes += $.app.elements.pane(x, y);
                 }
             }
 
             base.html("<div class='arena'>"+panes+"</div>");
 
-            $.app.setGridSize(size);
-
-            $.app.addApple(2, 5);
+            $.app.setGridSize();
         },
-        setGridSize: function(size) {
-            var gridsSetting = 'repeat('+size+', calc(100% / '+size+'))';
+        setGridSize: function() {
+            var gridsSetting = 'repeat('+GRID_SIZE+', calc(100% / '+GRID_SIZE+'))';
             $('.arena').css('grid-template-columns', gridsSetting);
             $('.arena').css('grid-template-rows', gridsSetting);
         },
@@ -62,6 +92,42 @@ $(function() {
             $('.pane.has-apple').html('');
             $('#pane-'+x+'-'+y).addClass('has-apple');
             $('#pane-'+x+'-'+y).html($.app.elements.apple);
+        },
+        setHead: function(x, y) {
+            CURR = { x: x, y: y};
+            console.log(CURR)
+
+            var previousHead = $('.pane.head');
+
+            if (previousHead.length) previousHead.removeClass('head');
+
+            $('#pane-'+x+'-'+y).addClass('head');
+        },
+        loop: function () {
+            if (STATE === STATES.ACTIVE) {
+
+                switch (DIRECTION) {
+                    case DIRECTIONS.UP:
+                        $.app.setHead(CURR.x, CURR.y - 1); break;
+                    case DIRECTIONS.DOWN:
+                        $.app.setHead(CURR.x, CURR.y + 1); break;
+                    case DIRECTIONS.LEFT:
+                        $.app.setHead(CURR.x + 1, CURR.y); break;
+                    case DIRECTIONS.RIGHT:
+                        $.app.setHead(CURR.x - 1, CURR.y); break;
+                }
+
+                $.app.check();
+
+                setTimeout(function () { $.app.loop() }, 700);
+                
+            } else if (STATE === STATES.LOSSER) {
+                $.app.scenes.losser();
+            }
+        },
+        check: function() {
+            if (CURR.x < 1 || CURR.x > GRID_SIZE || CURR.y < 1 || CURR.y > GRID_SIZE )
+                STATE = STATES.LOSSER;
         }
     }
 
